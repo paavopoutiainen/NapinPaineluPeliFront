19332a5dff7ef21537eba12a6031c37207e50947
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2020-02-27 20:34:47 +0200

    initialized the project, added simple webpack config with devserver

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+
+  return (
+    <div className="container">
+      hello
+    </div>
+  )
+}
+
+export default App
