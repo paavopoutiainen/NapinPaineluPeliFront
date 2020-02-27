5ca2b98e8351056e0c7f9d8a6c2ec9db50bef690
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2020-02-27 21:36:04 +0200

    added untilPoints state which is set after each click with the information received from server, rendering the untilPoints also

--- src/app.js
+++ src/app.js
@@ -5,9 +5,20 @@ const socket = io.connect("http://localhost:3001")
 
 const App = () => {
 
+    const [points, setPoints] = useState(20)
+    const [untilPoints, setUntilPoints] = useState(null)
+
     function handleClick() {
-        socket.emit("test")
+        socket.emit("click")
+        setPoints(points - 1)
     }
+
+    socket.on("clickResponse", (data) => {
+        if(data.pointsIncreased !== 0) {
+          setPoints(points + data.pointsIncreased)
+        }
+        setUntilPoints(data.clicksLeftBeforePoints)
+    })
 
   return (
         <div>
@@ -20,6 +31,14 @@ const App = () => {
                     40 points for every 100th<br></br>
                     and 250 points for every 500th click.<br></br>
                 </p>
+            </div>
+            <div id="gameStats">
+                <div id="points">
+                    <p><strong>Points: </strong>{points}</p>
+                </div>
+                <div id="clickInformation">
+                    <p><strong>Number of clicks to points at your last click: </strong>{untilPoints}</p>
+                </div>
             </div>
             <div id="button">
                 <button onClick={() => handleClick()}>Click me!!</button>
