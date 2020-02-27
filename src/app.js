756d939d60dba0e7d931f74e459b244d7e94f662
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2020-02-27 21:02:46 +0200

    added the instructions of the game inside p tags to the app.js

--- src/app.js
+++ src/app.js
@@ -11,8 +11,15 @@ const App = () => {
 
   return (
         <div>
-            <div className="container">
-                hello
+            <div id="instructions">
+                <p>Hey there, your job is to click the button. Every click will decrease your points by one.<br></br>
+                After each click you will be provided with the number of clicks before the next click resulting in increase in points.<br></br>
+              <br></br>
+                Points are given as follows: <br></br> 
+                    5 points for every 10th.<br></br>
+                    40 points for every 100th<br></br>
+                    and 250 points for every 500th click.<br></br>
+                </p>
             </div>
             <div id="button">
                 <button onClick={() => handleClick()}>Click me!!</button>
